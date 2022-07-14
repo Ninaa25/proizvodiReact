11f9e4fced6bba69d66d6ec86b13e78528bef2ba
Ninaa25 <ninamilutinovic8@gmail.com>
2022-07-14 15:51:22 +0200

    Napravljena forma na strani Create.js za dodavanje novog proizvoda, CSS

--- src/strane/Create.js
+++ src/strane/Create.js
@@ -1,12 +1,47 @@
 import NavBar from "../komponente/NavBar";
+import { useState } from 'react';
 
 const Create = () => {
+
+    const [id, setId] = useState('');
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    const idHandle = (e) => {
+        setId(e.target.value);
+    }
+
+    const titleHandle = (e) => {
+        setTitle(e.target.value);
+    }
+
+    const bodyHandle = (e) => {
+        setBody(e.target.value);
+    }
+
     return (
         <div className='create'>
             <NavBar />
 
+            <div className="frm-novi-proizvod">
 
+                <div className='element'>
+                    <label>ID</label>
+                    <input type="text" value={id} onChange={idHandle} />
+                </div>
 
+                <div className='element'>
+                    <label>Title</label>
+                    <input type="text" value={title} onChange={titleHandle} />
+                </div>
+
+                <div className='element'>
+                    <label>Body</label>
+                    <input type="text" value={body} onChange={bodyHandle} />
+                </div>
+
+                <button type="button" id="btn">Dodaj proizvod</button>
+            </div>
 
         </div>
     )
